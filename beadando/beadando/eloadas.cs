3e727d2c578f3e31a2da88ace2a902f9d78f5907
FoldiBencee <55578198+FoldiBencee@users.noreply.github.com>
2020-03-04 22:41:49 +0100

    Add files via upload

---
+++ beadando/beadando/eloadas.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace beadando
+{
+    public class Eloadas
+    {
+        bool[,] foglalasok = new bool[0, 0];
+
+        public Eloadas(int sorokSzama, int helyekSzama)
+        {
+            this.foglalasok = foglalasok;
+            if (sorokSzama < 1 || helyekSzama < 1)
+            {
+                throw new ArgumentException("A szamoknak pozitivnak kell lenniuk");
+            }
+            else
+            {
+                for (int i = 0; i < sorokSzama; i++)
+                {
+                    for (int j = 0; j < helyekSzama; j++)
+                    {
+                        foglalasok[i, j] = false;
+                    }
+                }
+            }
+        }
+
+            public bool Lefoglal()
+            {
+
+                for (int i = 0; i < foglalasok.GetLength(0); i++)
+                {
+                    for (int j = 0; j < foglalasok.GetLength(1); j++)
+                    {
+                        if (foglalasok[i, j] == false)
+                        {
+                            foglalasok[i, j] = true;
+                        return true;
+                        }
+                    }
+                }
+                return false;
+            }
+
+        public int SzabadHelyek()
+        {
+            int szabadHelyekSzama = 0;
+            for (int i = 0; i < foglalasok.GetLength(0); i++)
+            {
+                for (int j = 0; j < foglalasok.GetLength(1); j++)
+                {
+                    if (foglalasok[i, j] == false)
+                    {
+                        szabadHelyekSzama++;
+                    }
+                }
+            }
+            return szabadHelyekSzama;
+        }
+
+        public bool teli()
+        {
+            if (SzabadHelyek() == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Foglalt(int sorszam, int helyszam)
+        {
+            if (sorszam < 1 || helyszam < 1)
+            {
+                throw new ArgumentException("a szamoknak pozitivnak kell nelliuk");
+            }
+            else if (foglalasok[sorszam, helyszam] == true)
+            {
+                return false;
+            }
+            return true;
+        }
+
+
+    }
+    }
+}
+
---
+++ beadando/beadando/tests/eloadasoktest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using NUnit.Framework;
+namespace beadando.tests
+{
+    [TestFixture]
+    public class eloadasoktest
+    {
+        Eloadas e;
+        [SetUp]
+        public void SetUp()
+        {
+        e = new Eloadas(55, 55);
+        }
+
+        [TestCase]
+        public void MindLefoglal()
+        {
+            for (int i = 0; i < 55 * 55; i++)
+            {
+                e.Lefoglal();
+
+            }
+            Assert.AreEqual(true, e.teli());
+        }
+
+        [TestCase]
+        public void Foglalas()
+        {
+            e.Lefoglal();
+            Assert.AreEqual(10, e.SzabadHelyek());
+
+        }
+
+
+
+        [TestCase]
+        public void FoglaltHely()
+        {
+            e.Lefoglal();
+            Assert.AreEqual(false, e.Foglalt(22, 33));
+        }
+
+
+
+        [TestCase]
+        public void Foglalaska()
+        {
+            Assert.IsTrue(e.Lefoglal());
+        }
+
+        [TestCase]
+        public void tobbetnefoglalhasson()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+
+
+                e.Foglalt(9, 66);
+            });
+        }
+
+        [TestCase]
+
+
+        void Negativletrehozas()
+        {
+            var rep = new Eloadas(-44, -55);
+
+        }
+
+
+    }
+}
